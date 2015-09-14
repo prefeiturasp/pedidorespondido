3d2bb0de3b82787b74d57c1588ca9e7b3157d4f7
campagnucci <campagnucci@gmail.com>
2015-09-14 01:35:33 -0300

    Adiciona expressão regular para id numérico

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   Blacklight::Marc.add_routes(self)
   devise_for :users
 
-  get "/:id" => "catalog#show"
+  get "/:id" => "catalog#show", :constraints => {:id => /[0-9]+/}
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
